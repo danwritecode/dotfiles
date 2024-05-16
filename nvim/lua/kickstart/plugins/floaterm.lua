95e97e324a001c51fb9e6ea435a039ff3948bb2a
danwritecode <dnelson4993@outlook.com>
2024-05-16 15:16:01 -0400

    removed unused dirs

--- nvim/lua/kickstart/plugins/floaterm.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'numToStr/FTerm.nvim',
-  version = '*',
-  config = function()
-    vim.keymap.set('n', '<F7>', '<CMD>lua require("FTerm").toggle()<CR>')
-    vim.keymap.set('t', '<F7>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
-  end,
-}
