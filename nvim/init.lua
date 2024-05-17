17fe2c12f080744396bc0c38c8c48b18c3c5ae54
danwritecode <dnelson4993@outlook.com>
2024-05-16 22:10:01 -0400

    added blankline

--- nvim/init.lua
+++ nvim/init.lua
@@ -102,6 +102,9 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- changing colorscheme
+vim.keymap.set('n', '<leader>cc', '<cmd>Telescope colorscheme<CR>')
+
 -- Splitting windows
 vim.keymap.set('n', '<leader>v', '<cmd>vsplit<CR>')
 -- Keybinds to make split navigation easier.
@@ -684,20 +687,20 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  -- COLOR SCHEMES HERE
+  {
     'rebelot/kanagawa.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    priority = 1000,
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'kanagawa-dragon'
-
-      -- You can configure highlights by doing something like:
+      -- vim.cmd.colorscheme 'kanagawa-dragon'
+      -- vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000,
+    init = function()
+      vim.cmd.colorscheme 'tokyonight-night'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
@@ -789,6 +792,7 @@ require('lazy').setup({
   require 'plugins.bufferline',
   require 'plugins.neo-tree',
   require 'plugins.floaterm',
+  require 'plugins.blankline',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
